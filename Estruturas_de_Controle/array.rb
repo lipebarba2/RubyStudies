0b79fbfb96410adff39b92684d0a1c444d078b3b
LipeLimaFe <felipecinemkt@gmail.com>
2024-08-19 13:38:55 -0300

    feature array e hash

---
+++ Estruturas_de_Controle/array.rb
@@ -0,0 +1,7 @@
+arr = [1,2,3]
+print arr << 4
+print arr.push(5)
+print arr.unshift(0)
+puts"======================="
+arr1 = [6,7,8,9]
+print arr + arr1
---
+++ Estruturas_de_Controle/hash.rb
@@ -0,0 +1,21 @@
+hash = {name: "felipe", name2: 'bia', name3: "taina"}
+hash[:name4] = "kim"
+puts(hash)
+puts "----------------------------------"
+hash.each do |key,value|
+    puts "#{key}: #{value}"
+end
+
+puts "----------------------------------"
+hash2 = {name5: "Sofia"}
+com_hash = hash.merge(hash2)
+puts(com_hash)
+
+puts "----------------------------------"
+
+puts hash.fetch(:name2)
+puts hash.value?("Sofia")
+puts "----------------------------------"
+
+puts hash.invert
+puts "----------------------------------"
